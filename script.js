13e33a127d07b52106389679a1f3e030faeba47a
nathan Trefois <nathan.trefois@gmail.com>
2023-11-21 19:25:08 +0100

    initial push

--- script.js
+++ script.js
@@ -25,10 +25,10 @@ console.log('Exercice 5');
             return cardContainer;
         }
 
-    let cardv1 = createCard("Tarte aux pommes", "https://via.placeholder.com/158", "./recettes/tarte-aux-pommes.html", "15 minutes de preparation");
-    let cardv2 = createCard("Gateau au chocolat", "https://via.placeholder.com/158", "./recettes/gateau-au-chocolat.html", "20 minutes de preparation");
-    let cardv3 = createCard("Tarte aux groseilles", "https://via.placeholder.com/158", "./recettes/tarte-aux-groseilles.html", "25 minutes de preparation");
-    let cardv4 = createCard("Muffins aux myrtilles", "https://via.placeholder.com/158", "./recettes/muffins-myrtilles.html", "20 minutes de preparation");
+    let cardv1 = createCard("Tarte aux pommes", "https://via.placeholder.com/158", "./recettes/tarte-aux-pommes.html", "15 minutes de préparation");
+    let cardv2 = createCard("Gateau au chocolat", "https://via.placeholder.com/158", "./recettes/gateau-au-chocolat.html", "20 minutes de préparation");
+    let cardv3 = createCard("Gateau marbré", "https://via.placeholder.com/158", "./recettes/tarte-aux-groseilles.html", "20 minutes de préparation");
+    let cardv4 = createCard("Tarte aux groseilles", "https://via.placeholder.com/158", "./recettes/muffins-myrtilles.html", "25 minutes de préparation");
 
     document.body.append(cardv1);
     document.body.append(cardv2);
